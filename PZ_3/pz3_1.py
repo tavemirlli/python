8a2b1f3bfc9812955036ab72707de3e79aa1fe65
tavemirlli <talemirli@gmail.com>
2023-11-06 21:05:45 +0300

    modi

---
+++ PZ_3/pz3_1.py
@@ -0,0 +1,30 @@
+# Вариант 19. Проверить истинность высказывания: "Среди данных трех целых чисел есть хотя бы одна пара совпадающих"while type(a) != int:try:
+a = (input('Введите первое число: '))
+while type(a)!=int:
+    try:
+        a = int(a)
+    except ValueError:
+        print('Неправильно ввели!')
+        a = (input('Введите первое число: '))
+b = (input('Введите второе число: '))
+while type(b)!=int:
+    try:
+        b = int(b)
+    except ValueError:
+        print('Неправильно ввели!')
+        b = (input('Введите второе число: '))
+c = (input('Введите третье число: '))
+while type(c)!=int:
+    try:
+        c = int(c)
+    except ValueError:
+        print('Неправильно ввели!')
+        c = (input('Введите третье число: '))
+ab = (a==b)
+bc = (b==c)
+ac = (a==c)
+abc = ab or bc or ac
+print("a равно b: ", ab)
+print("b равно c: ", bc)
+print("a равно c: ", ac)
+print('Есть одна пара совпадающих чисел: ', abc)
