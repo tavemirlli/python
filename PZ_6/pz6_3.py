75c5773d317e51478b6fe7769cc3c3ed9c0d516c
tavemirlli <talemirli@gmail.com>
2023-12-12 23:56:33 +0300

    add files

---
+++ PZ_6/pz6_3.py
@@ -0,0 +1,21 @@
+# Дан список размера N, все элементы которого, кроме последнего, упорядочены по возрастанию. Сделать список
+# упорядоченным, переместив последний элемент на новую позицию.
+from random import randint
+list2 = []
+N = input('Введите длину списка: ')
+while type(N)!=int:
+    try:
+        N = int(N)
+    except ValueError:
+        print('Неправильно ввели! ')
+        N = (input('Введите целое число: '))
+for i in range (N):
+    list2.append(randint(1,100))
+print(list2)
+new_element = list2[-1]
+sorted_list = sorted(list2[:-1])
+index = 0
+while index < len(sorted_list) and sorted_list[index] < new_element:
+    index += 1
+sorted_list.insert(index, new_element)
+print("Упорядоченный список с последним элементом на новой позиции:", sorted_list)
