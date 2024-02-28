acb6daff8f81c1de724fa544ea2ee0580acdb77c
tavemirlli <talemirli@gmail.com>
2024-02-28 09:49:13 +0300

    modi files

--- PZ_13/pz13_1.py
+++ PZ_13/pz13_1.py
@@ -11,7 +11,9 @@ print(matrix[2,1], matrix[2,2])
 last_column = len(matrix[0]) - 1 # находим индекст последнего столбца матрицы
 second_last_column = len(matrix[0]) - 2 # находим индекс предпоследнего столбца матрицы
 
-last_two_columns = lambda row: row[last_column] + row[second_last_column] # создание функции lambda (сложение двух последних элементов в каждой строке матрицы)
-average = sum(map(last_two_columns, matrix)) / (2 * len(matrix)) #  применение написанной функции к матрице и деление на количество элементов в 2 последних столбцах
+last_two_columns = lambda row: row[last_column] + row[second_last_column] # создание функции lambda (сложение двух
+# последних элементов в каждой строке матрицы)
+average = sum(map(last_two_columns, matrix)) / (2 * len(matrix)) #  применение написанной функции к матрице и деление на
+# количество элементов в 2 последних столбцах
 
 print("Среднее арифметическое последних двух столбцов матрицы:", average)
