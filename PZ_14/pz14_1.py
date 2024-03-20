ef4d6d2cb875d16c1dc2445a8b5fda8ebd483346
tavemirlli <talemirli@gmail.com>
2024-03-20 12:51:33 +0300

    add files

--- PZ_14/pz14_1.py
+++ PZ_14/pz14_1.py
@@ -5,11 +5,14 @@
 import re
 
 # Открываем исходный файл и считываем его содержимое
-with open("pazzl.html", "r") as file:
+with open('pazzl.html', 'r') as file:
     data = file.read()
 
 # Используем регулярное выражение для поиска всех изображений
 image_tags = re.findall(r'<img.*?>', data)
+lines_as_lists = list(map(lambda x: x, image_tags))
+numbered_lines = list(enumerate(lines_as_lists, start=1))
+list(map(lambda x: print(f'{x[0]}: {x[1]}'), numbered_lines))
 
 # Нахадом количество изображений
 image_count = len(image_tags)
