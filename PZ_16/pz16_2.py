ce6b2f9006bf735a18516e6c8e0ecac8c7996252
shesnarik <olegkovalev700@gmail.com>
2024-05-15 17:20:10 +0300

    add and modi files

---
+++ PZ_16/pz16_2.py
@@ -0,0 +1,38 @@
+# Вариант 19. Создание базового класса "Животное" и его наследование для создания классов "Собака", "Кошка". В классе
+# "Животное" будут общие методы, такие как "дышать" и "питаться", а классы наследники будут иметь свои уникальные
+# методы и свойства такие как "гавкать" и "мурлыкать".
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def breathe(self):
+        print(f"{self.name} дышит")
+
+    def eat(self):
+        print(f"{self.name} питается")
+
+
+class Dog(Animal):
+    def bark(self):
+        print(f"{self.name} гавкает")
+
+
+class Cat(Animal):
+    def meow(self):
+        print(f"{self.name} мурлыкает")
+
+
+# Пример использования и создания объектов
+d = Dog('Щеночек')
+c = Cat('Котенок')
+
+d.breathe()
+d.eat()
+d.bark()
+
+print()
+
+c.breathe()
+c.eat()
+c.meow()
